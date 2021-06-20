dd64ddb10e0c195cb3a390fc0f4fbcb03aceaace
yashwant-c <74008172+yashwant-c@users.noreply.github.com>
2021-06-20 10:29:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src"'+data_uri+'"/>'
+    });
+}
+
+console.log('ml5 version:',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0Yt7VXnH1/model.json',modelLoaded);
+
+function modelLoaded() {
+console.log('Model loaded!');
+}
+
+function speak(){
+var synth = window.speechSynthesis;
+speak_data_1 = "The first predication is " + predication_1;
+speak_data_2 = "And the second predication is " + predication_2;
+var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+synth.speak(utterThis); 
+}
